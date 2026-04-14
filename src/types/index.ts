4c29ba401348ad0c80a1b14450703e28976ca1fd
sc4stock-pixel <sc4stock@gmail.com>
2026-04-14 00:53:29 +0000

    added totalreturn250

--- src/types/index.ts
+++ src/types/index.ts
@@ -124,6 +124,8 @@ export interface BacktestResult {
   win_rate: number;
   expectancy: number;
   total_return: number;
+  total_return_250d?: number; 
+  total_return_500d?: number;
   sharpe: number;
   sortino: number;
   max_drawdown: number;
