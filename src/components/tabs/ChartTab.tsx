f5ce3644b5290544ac8a8933052f9ac4e419ff5f
sc4stock-pixel <sc4stock@gmail.com>
2026-05-01 22:51:01 +0800

    Add useState import to ChartTab component

--- src/components/tabs/ChartTab.tsx
+++ src/components/tabs/ChartTab.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { useState } from "react";
 import { StockAnalysisResult, ChartBar, TimesfmPriceTargets, AppConfig } from "@/types";
+import { supertrend } from "@/lib/indicators";
 import {
   ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid,
   Tooltip, ResponsiveContainer, ReferenceLine, Area,
